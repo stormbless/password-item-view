c6b3abd373995c4ad2df4883dde0e06d94227710
stormbless <75245920+stormbless@users.noreply.github.com>
2025-06-02 13:05:33 +1000

    Use React strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,14 @@ import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { StrictMode } from 'react';
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
